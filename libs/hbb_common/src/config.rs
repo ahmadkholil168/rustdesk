8a9900a1f4100b997ef2363c113122011f4587f0
ahmadkholil168 <61233192+ahmadkholil168@users.noreply.github.com>
2024-09-01 00:47:47 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.tzehong.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["121727850.xyz"];
+pub const PUBLIC_RS_PUB_KEY: &str = "tc38u7n9nykEuJfWpB4e2kzeL55hfn+2t7rrFUCQVqc=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
